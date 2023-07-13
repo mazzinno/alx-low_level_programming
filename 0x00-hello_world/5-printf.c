ac6f2af33283cece8e4c9d1d5cdb70ea705a994c
mazzinno <abdennasr3@gmail.com>
2023-07-13 16:25:41 -0700

    test

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - entry main point
+*return: always 0 (success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
