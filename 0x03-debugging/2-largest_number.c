dfcbcacd041409c2c62877e1b34c834f14aa79c6
mazzinno <abdennasr3@gmail.com>
2023-07-19 11:08:30 -0700

    testnow

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > c && b > c)
+	else if (b >= c && b >= c)
 	{
 		largest = b;
 	}
 	else
 	{
-	largest = c;
+		largest = c;
 	}
 
 	return (largest);
