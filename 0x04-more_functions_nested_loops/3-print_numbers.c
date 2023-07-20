1551627f0efdf0522eed82897233d393b430bab1
mazzinno <abdennasr3@gmail.com>
2023-07-20 07:06:40 -0700

    third

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - functions that print the numbers
+ *
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
+
