ee3da49cfea1764411224cb89e91426422451db0
mazzinno <abdennasr3@gmail.com>
2023-07-20 19:09:45 -0700

    ninsde

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,20 +11,16 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf("FizzBuzz");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz");
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz");
-		}
 		else
-		{
-			printf("%d ", i);
-		}
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
 	}
+	printf("\n");
+	return (0);
 }
