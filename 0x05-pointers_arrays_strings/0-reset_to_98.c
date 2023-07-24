723ec9e7d187714a1c9fb5f5ec9c3d0e8e0fc3c4
mazzinno <abdennasr3@gmail.com>
2023-07-24 09:17:50 -0700

    hehe

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - something here
+ * @n: pointer to make 98
+ *return: always 0
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+my redamefile for pointers
