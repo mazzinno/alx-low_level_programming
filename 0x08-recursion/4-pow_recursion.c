c4a0c9ddc2628820f5700c091771d1764c00cb7b
mazzinno <abdennasr3@gmail.com>
2023-08-03 05:39:34 -0700

    test

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,16 +1,26 @@
 #include "main.h"
 
 /**
- * _pow_recursion - factorial of a given number
- * @x: arg 1
- * @y: arg 2
- * Return: resault
+ * _pow_recursion - Value of x raised to the power of y
+ *
+ * @x: int arg
+ *
+ * @y: int arg
+ *
+ * Return: int
  */
+
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
+	{
 		return (-1);
+	}
+
 	if (y == 0)
+	{
 		return (1);
+	}
+
 	return (x * _pow_recursion(x, y - 1));
 }
