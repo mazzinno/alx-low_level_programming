c3c0d8ea49435d1a4296ce86f19b87e8e775b2bf
abdennacer <135185867+mazzinno@users.noreply.github.com>
2023-08-29 12:35:23 +0100

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,12 +8,12 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *temp = malloc(sizeof(listint_t));
+listint_t *ptr;
 
 if (temp == NULL)
 {
 	return (NULL);
 }
-listint_t *ptr;
 ptr = *head;
 temp->n = n;
 temp->next = NULL;
