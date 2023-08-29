ef714cdf51d97fa9009296f062b457e98211e308
mazzinno <abdennasr3@gmail.com>
2023-08-28 17:08:34 -0700

    test

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint - print linked list
+ * @h: head of linked list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+	const listint_t *ptr = NULL;
+
+	ptr = h;
+
+	if (ptr == NULL)
+	{
+	return (0);
+	}
+	while (ptr != NULL)
+	{
+	printf("%d \n", ptr->n);
+	ptr = ptr->next;
+	i++;
+	}
+	return (i);
+}
