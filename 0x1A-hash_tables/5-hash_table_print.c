a2ca763c433c4b611a0d43f213c6148878f713bb
mazzinno <abdennasr3@gmail.com>
2023-11-08 18:42:45 -0800

    done

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,1 +1,41 @@
-5-hash_table_print.c
+#include "hash_tables.h"
+
+/**
+ *  hash_table_print - Prints a hash table
+ *
+ *  @ht: The hash table
+ *
+ *  Return: The key/value pair according to example
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index = 0, flag = 0;
+
+	hash_node_t *head = NULL;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	printf("{");
+
+	while (ht->size > index)
+	{
+		head = ht->array[index];
+
+		while (head != NULL)
+		{
+			if (flag == 1)
+			{
+				printf(", ");
+			}
+
+			printf("'%s': '%s'", head->key, head->value);
+			flag = 1;
+			head = head->next;
+		}
+		index++;
+	}
+	printf("}\n");
+}
