43461ee2adbb152bd28ea04716b0a14bad4ae217
mazzinno <abdennasr3@gmail.com>
2024-03-17 05:58:56 +0000

    testing

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,7 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
